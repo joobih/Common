fc6800ccd124dc8d099fd7147add7ec63d0b8d0d
joobih <1006536507@qq.com>
2017-05-09 22:27:44 +0800

    增加uncode_str 编码装换

--- useful2.py
+++ useful2.py
@@ -3,6 +3,7 @@
 
 import re
 import time
+import json
 
 """
     判断ip是否是正确的ip地址True--是ip地址,False--不是ip地址
@@ -44,3 +45,9 @@ def revert_to_timestamp(date,div = '-'):
     time_stamp = time.mktime(_t)
     return time_stamp
 
+"""
+    讲字符串中包含有\u1234 字符的替换为原来的中文
+"""
+def unicode_to_gbk(u_str):
+    result = json.loads('{"str":"%s"}' % u_str)
+    return result["str"]
