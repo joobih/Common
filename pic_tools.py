c7960765c2d5be0d25ce826ff859ffb0a55a3474
joobih <1006536507@qq.com>
2017-05-10 21:48:41 +0800

    增加保存图片的功能

--- pic_tools.py
+++ pic_tools.py
@@ -10,7 +10,7 @@ import base64
         b64_pics:经过base64编码的图片数据
         smaller:图片缩小比例，取值范围为(0,1]
 """
-def paser_pic(b64_pics,smaller = 1):
+def paser_pic(b64_pics,save_path,smaller = 1):
     if sys.version < '3':
         from cStringIO import StringIO
         b64_pics = base64.b64decode(b64_pics)
@@ -19,9 +19,9 @@ def paser_pic(b64_pics,smaller = 1):
         width,height = im.size
         if smaller != 1:
             small_img = im.resize((int(width*smaller), int(height*smaller)),Image.ANTIALIAS)
-            b64_small_pic = base64.b64encode(small_img.getvalue())
-            return (width,height,b64_small_pic)
-        return (width,height,b64_pics)
+            small_img.save(save_path["small_path"])
+        im.save(save_path["big_path"])
+        return (width,height)
     else:
         from io import BytesIO
         b64_pics = base64.b64decode(b64_pics)
@@ -31,12 +31,13 @@ def paser_pic(b64_pics,smaller = 1):
         width,height = im.size
         if smaller != 1:
             small_img = im.resize((int(width*smaller), int(height*smaller)),Image.ANTIALIAS)
-            b64_small_pic = base64.b64encode(small_img.getvalue())
-            return (width,height,b64_small_pic)
-        return (width,height,b64_pics)
+            small_img.save(save_path["small_path"])
+        im.save(save_path["big_path"])
+        return (width,height)
 
 #f = "a.jpg"
 #b = open(f,"rb")
 #c = b.read()
 #pic = base64.b64encode(c)
-#print(paser_pic(pic))
+#save_path = {"small_path":"s.jpg","big_path":"b.jpg"}
+#print(paser_pic(pic,save_path,0.5))
